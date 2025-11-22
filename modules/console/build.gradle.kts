ed9ad8e40b2aad6e336c7325734243267c3fbcab
Phong Chu <chucamphong1999@gmail.com>
2025-11-22 08:31:15 +0700

    feat: Upgrade to Spring Boot 4.0.0 and adjust configurations
    - Update Spring Boot version from 4.0.0-RC2 to 4.0.0 in libs.versions.toml
    - Change GraalVM native plugin version to 0.11.3
    - Modify Java indentation settings in .editorconfig from 2 to 4 spaces
    - Add missing newline at the end of build.gradle.kts
    - Implement additional test dependencies in console module
    - Create initial test class for WorkastraConsoleApplication

--- modules/console/build.gradle.kts
+++ modules/console/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
   java
-  alias(libs.plugins.spring.boot) 
+  alias(libs.plugins.spring.boot)
   alias(libs.plugins.spring.dependency.management)
-  alias(libs.plugins.graalvm.native) 
+  alias(libs.plugins.graalvm.native)
 }
 
 dependencies {
   implementation(project(":modules:common"))
-  implementation("org.springframework.boot:spring-boot-starter-webmvc")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation("org.springframework.boot:spring-boot-starter-webmvc")
+  testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ modules/console/src/test/java/com/workastra/console/WorkastraConsoleApplicationTest.java
@@ -0,0 +1,11 @@
+package com.workastra.console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WorkastraConsoleApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
